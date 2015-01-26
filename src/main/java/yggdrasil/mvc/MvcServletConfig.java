7be8692238480449bc7ea477730e7a1fb4a3a537
Jason Steele <jasonesteele@gmail.com>
2015-01-26 15:04:56 -0500

    Adding configuration and API version for swagger

--- src/main/java/yggdrasil/mvc/MvcServletConfig.java
+++ src/main/java/yggdrasil/mvc/MvcServletConfig.java
@@ -1,8 +1,12 @@
 package yggdrasil.mvc;
 
+import javax.annotation.Resource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,7 +15,10 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
 import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
 
 /**
  * Configuration for Spring MVC dispatcher servlet.
@@ -22,7 +29,14 @@ import com.mangofactory.swagger.plugin.EnableSwagger;
 @EnableSwagger
 @Configuration
 @ComponentScan
+@PropertySource("classpath:swagger.properties")
 public class MvcServletConfig extends WebMvcConfigurerAdapter {
+  @Resource
+  private Environment env;
+
+  @Resource
+  private SpringSwaggerConfig springSwaggerConfig;
+
   @Override
   public void addResourceHandlers(final ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/static/css/")
@@ -41,6 +55,13 @@ public class MvcServletConfig extends WebMvcConfigurerAdapter {
     registry.addViewController("/admin/user_mgmt").setViewName("admin/user_mgmt");
   }
 
+  @Bean
+  public ApiInfo apiInfo() {
+    return new ApiInfo("Yggdrasil REST API", "REST API for Yggdrasil back-end",
+        "Yggdrasil Terms of Service", "ygdrassil-dev@darthgeek.us", "Yggdrasil License",
+        "Yggdrasil License");
+  }
+
   @Override
   public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
     configurer.enable();
@@ -52,5 +73,15 @@ public class MvcServletConfig extends WebMvcConfigurerAdapter {
     resolver.setPrefix("/WEB-INF/jsp/");
     resolver.setSuffix(".jsp");
     return resolver;
+  }
+
+  @Bean
+  public SwaggerSpringMvcPlugin swaggerPlugin() {
+    final SwaggerSpringMvcPlugin swaggerPlugin = new SwaggerSpringMvcPlugin(springSwaggerConfig)
+        .apiInfo(apiInfo()).includePatterns("/api/*", "/admin/api/.*");
+
+    swaggerPlugin.apiVersion(env.getProperty("swagger.apiVersion", "0.0"));
+
+    return swaggerPlugin;
   }
 }
