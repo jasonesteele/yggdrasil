1d55380f76dd2407b935eff08ce07bf1413d37ed
Jason Steele <jasonesteele@gmail.com>
2022-05-18 00:53:15 -0400

    Removing invalid test

--- test/jest/AccessDenied.spec.js
+++
@@ -1,9 +0,0 @@
-import AccessDenied from "../../src/components/AccessDenied";
-import { cleanup, render, screen } from "@testing-library/react";
-
-afterEach(cleanup);
-
-it("displays not authorized text", () => {
-  render(<AccessDenied />);
-  expect(screen.getByTitle("Access Denied")).toBeVisible();
-});
