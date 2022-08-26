123cadca2f8c32dd692ce79dd747d3a2b083e2de
Jason Steele <jasonesteele@gmail.com>
2022-08-26 15:50:44 -0400

    Toning logging down for prod

--- src/util/prisma.ts
+++ src/util/prisma.ts
@@ -9,7 +9,7 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ["event", "query"],
+    log: process.env.NODE_ENV !== "production" ? ["query"] : [],
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
