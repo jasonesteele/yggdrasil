322807d3aaaf0feb274633f3b72160820c196bf2
Jason Steele <jasonesteele@gmail.com>
2022-09-01 19:01:27 -0400

    Fixing test paths

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,7 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["./jest.setup.js"],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/cypress/',
-  ]
+  testPathIgnorePatterns: ["/node_modules/", "/cypress/", "/dist/"],
 };
 
 module.exports = createJestConfig(customJestConfig);
