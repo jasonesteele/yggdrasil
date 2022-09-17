526cca3b4722275988786ff99aa01bd337c46bda
Jason Steele <jasonesteele@gmail.com>
2022-09-16 21:22:35 -0400

    Removing extranneous  jest configs

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-const customJestConfig = {
-  moduleDirectories: ["node_modules", "<rootDir>/"],
-  setupFilesAfterEnv: ["./jest.setup.js"],
-  testPathIgnorePatterns: ["/node_modules/", "/cypress/", "/dist/", "/client/"],
-};
-
-module.exports = customJestConfig;
