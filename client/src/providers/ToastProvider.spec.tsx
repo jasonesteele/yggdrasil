251343e9125adbf7806f6421e428157edcf9e8ef
Jason Steele <jasonesteele@gmail.com>
2022-09-19 19:18:01 -0400

    ToastProvider unit test

--- client/src/providers/ToastProvider.spec.tsx
+++ client/src/providers/ToastProvider.spec.tsx
@@ -1,15 +1,40 @@
-import { render } from "@testing-library/react";
-import ToastProvider from "./ToastProvider";
+import { Button } from "@mui/material";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ToastProvider, { useToastContext } from "./ToastProvider";
 
 describe("providers", () => {
   describe("ToastProvider", () => {
-    it("provides toast services", () => {
-      // TODO: implement me
+    it("provides toast services", async () => {
+      const Component = () => {
+        const { showToast } = useToastContext();
+
+        return (
+          <Button
+            onClick={() =>
+              showToast({ severity: "error", message: "toast message" })
+            }
+          >
+            Show
+          </Button>
+        );
+      };
       render(
         <ToastProvider>
-          <div>Hello</div>
+          <Component />
         </ToastProvider>
       );
+
+      expect(screen.queryByText("toast message")).not.toBeInTheDocument();
+      userEvent.click(screen.getByText("Show"));
+      expect(screen.getByText("toast message")).toBeInTheDocument();
+      userEvent.click(screen.getByTestId("CloseIcon"), {
+        bubbles: true,
+        cancelable: true,
+      });
+      await waitFor(() => {
+        expect(screen.queryByText("toast message")).not.toBeInTheDocument();
+      });
     });
   });
 });
